5c7e83f9ec51b4d0aa786fa45a506aa5d584f7c0
Carlos Macías Sandoval <carlossan.16.mc@gmail.com>
2026-09-02 17:36:27 -0400

    add example of complex function

--- src/clean-code/03-functions.ts
+++ src/clean-code/03-functions.ts
@@ -45,4 +45,23 @@
         return response;
     }
 
+      const getPayAmount = ({ isDead = false, isSeparated = true, isRetired = false }) => {
+        let result;
+        if ( isDead ) {
+            result = 1500;
+        } else {
+            if ( isSeparated ) {
+                result = 2500;
+            } else {
+                if ( isRetired ) {
+                    result = 3000;
+                } else {
+                    result = 4000; 
+                }
+            }
+        }
+        
+        return result;
+    }
+
 })();
