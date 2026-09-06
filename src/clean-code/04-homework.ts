f13b7ab0efa2520b43708ee4832374d5cf083d1a
Carlos Macías Sandoval <carlossan.16.mc@gmail.com>
2026-09-05 22:39:35 -0400

    functions homework done

--- src/clean-code/04-homework.ts
+++ src/clean-code/04-homework.ts
@@ -4,56 +4,59 @@
     // includes? arrays?
     function isRedFruit( fruit: string ): boolean {
         
-        if ( fruit === 'manzana' || fruit === 'cereza' || fruit === 'ciruela' ) {
-            return true;
-        } else {
-            return false;
-        }
+        const redFruits = ['manzana', 'cereza', 'ciruela'];
+        return redFruits.includes(fruit);
+
     }
 
     // Simplificar esta función
     // switch? Object literal? validar posibles colores
-    function getFruitsByColor( color: string ): string[] {
 
-        if ( color === 'red' ) {
-            return ['manzana','fresa'];
-        } else if ( color === 'yellow') {
-            return ['piña','banana'];
-        } else if ( color === 'purple') {
-            return ['moras','uvas']
-        } else {
-            throw Error('the color must be: red, yellow, purple');
-        }
+    // Extra en typescript para declarar valores limitados estrictos.
+    type FruitColor = 'red'|'yellow'|'purple';
+
+    function getFruitsByColor( color: FruitColor ): string[] {
+
+        //* Mi solución:
+        const redFruits     = ['manzana','fresa'];
+        const yellowFruits  = ['piña','banana'];
+        const purpleFruits  = ['moras','uvas'];
+
+        if ( color === 'red' )      return redFruits;
+        if ( color === 'yellow' )   return yellowFruits;
+        if ( color === 'purple' )   return purpleFruits;
+
+        throw Error('the color must be: red, yellow, purple');
+
+
+        //* Solución de Fernando:
+        // const fruitByColor = {
+        //     red:    ['manzana','fresa'],
+        //     yellow: ['piña','banana'],
+        //     purple: ['moras','uvas'],
+        // }
+
+        // if ( !Object.keys( fruitByColor ).includes(color) ) {
+        //     throw Error('the color must be: red, yellow, purple');
+        // }
+
+        // return fruitByColor[color];
     }
 
     // Simplificar esta función
-    let isFirstStepWorking  = true;
-    let isSecondStepWorking = true;
-    let isThirdStepWorking  = true;
-    let isFourthStepWorking = true;
+    const   isFirstStepWorking  = true,
+            isSecondStepWorking = true,
+            isThirdStepWorking  = true,
+            isFourthStepWorking = true;
 
     function workingSteps() {
-        if( isFirstStepWorking === true ) {
-            if( isSecondStepWorking === true ) {
-                if( isThirdStepWorking === true ) {
-                    if( isFourthStepWorking === true ) {
-                        return 'Working properly!';
-                    }
-                    else {
-                        return 'Fourth step broken.';
-                    }
-                }
-                else {
-                    return 'Third step broken.';
-                }
-            }
-            else {
-                return 'Second step broken.';
-            }
-        }
-        else {
-            return 'First step broken.';
-        }
+        
+        if( !isFirstStepWorking )   return 'First step broken.';
+        if( !isSecondStepWorking )  return 'Second step broken.';
+        if( !isThirdStepWorking )   return 'Third step broken.';
+        if( !isFourthStepWorking )  return 'Fourth step broken.'
+
+        return 'Working properly!';
     }
 
 
@@ -65,7 +68,7 @@
     console.log({ redFruits: getFruitsByColor('red') }); // ['manzana', 'fresa']
     console.log({ yellowFruits: getFruitsByColor('yellow') }); // ['piña', 'banana']
     console.log({ purpleFruits: getFruitsByColor('purple') }); // ['moras', 'uvas']
-    console.log({ pinkFruits: getFruitsByColor('pink') }); // Error: the color must be: red, yellow, purple
+    // console.log({ pinkFruits: getFruitsByColor('pink') }); // Error: the color must be: red, yellow, purple
 
     // workingSteps
     console.log({ workingSteps: workingSteps() }); // Cambiar los valores de la línea 31 y esperar los resultados
