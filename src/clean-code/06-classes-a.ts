7e17b8a4df52941783bba9fb93f210ca27d400fd
Carlos Macías Sandoval <carlossan.16.mc@gmail.com>
2026-09-06 18:52:09 -0300

    example of NOT USING the Single Responsibility Principle
    > In this example we can see how hard is to use and maintain a class that extends another class nesting deeply, breaking the Single Responsibility Principle at the final class because it does so much things

--- src/clean-code/06-classes-a.ts
+++ src/clean-code/06-classes-a.ts
@@ -3,22 +3,60 @@
     type Gender = 'M'|'F';
 
     class Person {
-        // public name: string;
-        // public gender: Gender;
-        // public birthDate: Date;
 
         constructor(
             public name: string, 
             public gender: Gender, 
             public birthDate: Date
+        ) {}
+    }
+
+    class User extends Person {
+        constructor(
+            public email: string,
+            public role: string,
+            private lastAccess: Date,
+            name: string,
+            gender: Gender,
+            birthDate: Date,
         ) {
-            // this.name = name
-            // this.gender = gender
-            // this.birthDate = birthDate
+            super(name, gender, birthDate)
+        }
+        
+        checkCredentials(){
+            return true;
+        }      
+    }
+
+    class UserSettings extends User {
+        constructor(
+            public workingDirectory: string,
+            public lastOpenFolder: string,
+            email: string,
+            role: string,
+            lastAccess: Date,
+            name: string,
+            gender: Gender,
+            birthDate: Date,
+        ) {
+            super( email, role, lastAccess, name, gender, birthDate )
         }
     }
 
-    const newPerson = new Person('Carlos', 'F', new Date('1994-09-04'));
-    console.log(newPerson);
+    const newUserSettings = new UserSettings(
+        '/user/home',
+        '/home',
+        'crlos@gmail...',
+        'Admin',
+        new Date('2026-09-06'),
+        'Carlos',
+        'F',
+        new Date('1994-09-04'),
+    );
+    console.log({
+        newUserSettings,
+        areCredentialsValid: newUserSettings.checkCredentials()
+    });
+    
 
 })();
