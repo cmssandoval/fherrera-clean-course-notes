cac4b1c5abffd7ee45af198acc6224262c9cc28f
Carlos Macías Sandoval <carlossan.16.mc@gmail.com>
2026-09-06 14:34:20 -0300

    implement a validation method following the DRY principle
    > This makes so much easy to scale validation method than copying and pasting if blocks, even having an bigger initial code structure.

--- src/clean-code/05-dry.ts
+++ src/clean-code/05-dry.ts
@@ -8,15 +8,32 @@ class Product {
         public size: Size = '',
     ){}
 
+
+    isProductReady(): boolean {
+        for (const key in this) {
+            switch (typeof this[key]) {
+                case 'string':
+                    if (this[key].length <= 0) throw Error(`${key} is empty`);
+                    break;
+                case 'number':
+                    if (this[key] <= 0) throw Error(`${key} is zero`);
+                    break;
+                default:
+                    throw Error(`${ typeof this[key] } is not valid`);
+            }
+        }
+        return true;
+    }
+
     toString() {
         // No DRY
-        if (this.name.length <= 0 ) throw Error('name is empty');
-        if (this.price <= 0 ) throw Error('price is zero');
-        if (this.size.length <= 0 ) throw Error('size is empty');
+        // if (this.name.length <= 0 ) throw Error('name is empty');
+        // if (this.price <= 0 ) throw Error('price is zero');
+        // if (this.size.length <= 0 ) throw Error('size is empty');
+        if ( !this.isProductReady() ) return;
 
         return `${ this.name } (${ this.price }), ${ this. size }`;
     }
-
 }
 
 (() => {
