3fdf9fac673f282614c43b6890085fdcb13c0782
Carlos Macías Sandoval <carlossan.16.mc@gmail.com>
2026-09-08 15:27:56 -0300

    convert a multiple responsibility class into different classes following SRP
    > Using each other classes methods via Dependency Injection (pass other classes instances to the main class constructor as arguments)

--- src/solid/01-srp.ts
+++ src/solid/01-srp.ts
@@ -5,36 +5,76 @@
         name: string;
     }
     
-    // Usualmente, esto es una clase para controlar la vista que es desplegada al usuario
-    // Recuerden que podemos tener muchas vistas que realicen este mismo trabajo.
-    class ProductBloc {
-    
-        loadProduct( id: number ) {
-            // Realiza un proceso para obtener el producto y retornarlo
+    class ProductService {
+
+        private httpAdapter: Object = {};
+
+        getProduct( id: number){
             console.log('Producto: ',{ id, name: 'OLED Tv' });
         }
-    
+
         saveProduct( product: Product ) {
             // Realiza una petición para salvar en base de datos 
             console.log('Guardando en base de datos', product );
         }
-    
-        notifyClients() {
-            console.log('Enviando correo a los clientes');
-        }
-    
-        onAddToCart( productId: number ) {
-            // Agregar al carrito de compras
-            console.log('Agregando al carrito ', productId );
+        
+    }
+
+    class EmailSender {
+        private originEmail: string = 'asdasdas@asd.asd';
+            
+        sendEmail( emailList:string[], message: string ) {
+            console.log('Enviando correo a los clientes', emailList, message);
         }
     
     }
 
-    const productBloc = new ProductBloc();
+    // Usualmente, esto es una clase para controlar la vista que es desplegada al usuario
+    // Recuerden que podemos tener muchas vistas que realicen este mismo trabajo.
+    class ProductBloc {
+
+        private productService: ProductService;
+        private emailSender: EmailSender;
+    
+        constructor( productService: ProductService, emailSender: EmailSender ){
+            this.productService = productService;
+            this.emailSender = emailSender;
+        }
+
+        loadProduct( id: number ) {
+            this.productService.getProduct( id );
+        }
+    
+        saveProduct( product: Product ) {
+            this.productService.saveProduct( product );
+        }
+
+        notifyClients( emailList:string[], message: string ) {
+            this.emailSender.sendEmail( emailList, message );
+        }
+
+    }
+
+    class CartBloc{
+
+        private itemsInCart: Object[] = [];
+
+        addToCart( productId: number ) {
+            console.log('Agregando al carrito ', productId );
+        }
+
+    }
+    
+    const productService = new ProductService();
+    const emailSender = new EmailSender();
+
+    // Dependencies Injection keep each class with a single responsibility.
+    const productBloc = new ProductBloc( productService, emailSender);
+    const cartBloc = new CartBloc();
 
     productBloc.loadProduct(10);
     productBloc.saveProduct({ id: 10, name: 'OLED TV' });
-    productBloc.notifyClients();
-    productBloc.onAddToCart(10);
+    emailSender.sendEmail(['asdasd@gmail.com'], 'Hola clientes');
+    cartBloc.addToCart(10);
 
 })();
