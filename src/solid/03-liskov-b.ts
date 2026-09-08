536d04fb6bf47a87eff83799bf999ab5111bb3bb
Carlos Macías Sandoval <carlossan.16.mc@gmail.com>
2026-09-08 17:45:46 -0300

    add Vehicle abstract class with an abstract method
    > This is how you can fix a Liskov Substitution Principle Violation.
    > After creating the abstract class, extend it to the other classes.
    > Call the super at each one.
    > In this example, the parameter data definition was changed to Vehicle type.
    > The for of loop was refactored to an Array.forEach method.
    > You even could keep the old specific method for each subclass without any problem.

--- src/solid/03-liskov-b.ts
+++ src/solid/03-liskov-b.ts
@@ -1,37 +1,72 @@
 
+export abstract class Vehicle {
 
-export class Tesla {
+    abstract getNumberOfSeats(): number;
 
-    constructor( private numberOfSeats: number ) {}
+}
 
-    getNumberOfTeslaSeats() {
+export class Tesla extends Vehicle {
+
+    constructor( private numberOfSeats: number ) {
+        super();
+    }
+
+    getNumberOfSeats() {
         return this.numberOfSeats;
     }
 }
 
-export class Audi {
+export class Audi extends Vehicle {
 
-    constructor( private numberOfSeats: number ) {}
+    constructor( private numberOfSeats: number ) {
+        super();
+    }
 
-    getNumberOfAudiSeats() {
+    getNumberOfSeats() {
         return this.numberOfSeats;
     }
 }
 
-export class Toyota {
+export class Toyota extends Vehicle {
 
-    constructor( private numberOfSeats: number ) {}
+    constructor( private numberOfSeats: number ) {
+        super();
+    }
 
-    getNumberOfToyotaSeats() {
+    getNumberOfSeats() {
         return this.numberOfSeats;
     }
 }
 
-export class Honda {
+export class Honda extends Vehicle {
 
-    constructor( private numberOfSeats: number ) {}
+    constructor( private numberOfSeats: number ) {
+        super();
+    }
 
-    getNumberOfHondaSeats() {
+    getNumberOfSeats() {
+        return this.numberOfSeats;
+    }
+}
+
+export class Volvo extends Vehicle {
+
+    constructor( private numberOfSeats: number ) {
+        super();
+    }
+
+    getNumberOfSeats() {
+        return this.numberOfSeats;
+    }
+}
+
+export class Ford extends Vehicle {
+
+    constructor( private numberOfSeats: number ) {
+        super();
+    }
+
+    getNumberOfSeats() {
         return this.numberOfSeats;
     }
 }
